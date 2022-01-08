6ea73c9bee3f83f165e87df69f8358ad13db2cf8
zhangzan0224 <50470425+zhangzan0224@users.noreply.github.com>
2022-01-08 23:20:20 +0800

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module'
-  },
   extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   productionSourceMap: false,
   // 关闭ESLINT校验工具
-  lintOnSave: false,
+  // lintOnSave: false,
   // 配置代理跨域
   devServer: {
     proxy: {
